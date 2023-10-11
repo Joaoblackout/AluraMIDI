770d824ee34fb883ef4d84090e4ec40988803948
Joaoblackout <132076835+Joaoblackout@users.noreply.github.com>
2023-10-11 08:45:42 -0300

    Add files via upload

---
+++ Pedro e Jão F/main.js
@@ -0,0 +1,26 @@
+function tocaSomBrasil(){
+    document.querySelector('#som_tecla_brasil').play();
+}
+
+document.querySelector('.tecla_brasil').onclick = tocaSomBrasil;
+
+
+function tocaSomMario(){
+    document.querySelector('#som_tecla_mario').play();
+}
+
+document.querySelector('.tecla_mario').onclick = tocaSomMario;
+
+
+function tocaSomPlantao(){
+    document.querySelector('#som_tecla_plantao').play();
+}
+
+document.querySelector('.tecla_plantao').onclick = tocaSomPlantao;
+
+
+function tocaSomShowDoMilhao(){
+    document.querySelector('#som_tecla_show').play();
+}
+
+document.querySelector('.tecla_show').onclick = tocaSomShowDoMilhao;
